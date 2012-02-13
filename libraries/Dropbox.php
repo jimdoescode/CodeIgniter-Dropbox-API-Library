227150c61ad9a0b5db98977e34c42b2eafdb57f1
Jim <jimdoescode@gmail.com>
2012-02-13 17:01:57 -0500

    Updated library files to comply with CodeIgniter standards

--- libraries/Dropbox.php
+++ libraries/Dropbox.php
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-class dropbox
+class Dropbox
 {
     const SCHEME        = 'https';
     const HOST          = 'api.dropbox.com';
